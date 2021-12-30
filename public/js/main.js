aacc759b2529840124741394dcfc67b6a53044bb
Fabio Teixeira <fabio.teixeira@ifsp.edu.br>
2021-12-30 01:25:28 -0300

    Adequação à aula.

--- public/js/main.js
+++ public/js/main.js
@@ -1,26 +1,26 @@
-angular.module('ifsp', ['ngRoute']).config(function($routeProvider) {
-	
-	$routeProvider.when('/contatos', {
-		templateUrl: 'partials/contatos.html',
-		controller: 'ContatosController'
-	});
+angular.module('ifsp', ['ngRoute', 'ngResource']).config(function($routeProvider) {
 
-	$routeProvider.when('/contato/:contatoId', {
-    	templateUrl: 'partials/contato.html', 
-    	controller: 'ContatoController'
+    $routeProvider.when('/contatos', {
+        templateUrl: 'partials/contatos.html',
+        controller: 'ContatosController'
+    });
+
+    $routeProvider.when('/contato/:contatoId', {
+        templateUrl: 'partials/contato.html',
+        controller: 'ContatoController'
     });
 
     $routeProvider.when('/cursos', {
-		templateUrl: 'partials/cursos.html',
-		controller: 'CursosController'
-	});
+        templateUrl: 'partials/cursos.html',
+        controller: 'CursosController'
+    });
 
-	$routeProvider.when('/curso/:contatoId', {
-    	templateUrl: 'partials/curso.html', 
-    	controller: 'CursoController'
-    });   
+    $routeProvider.when('/curso/:contatoId', {
+        templateUrl: 'partials/curso.html',
+        controller: 'CursoController'
+    });
 
 
-    $routeProvider.otherwise({redirectTo: '/contatos'});
+    $routeProvider.otherwise({ redirectTo: '/contatos' });
 
 });
