a61ca9804dda794a0ab459c3c26ad7bfa59b1221
Victor Paes <victor.paes@aluno.ifsp.edu.br>
2022-03-24 21:11:53 -0300

    adjusts

--- public/js/controllers/CursoController.js
+++ public/js/controllers/CursoController.js
@@ -2,15 +2,12 @@ angular.module('ifsp').controller('CursoController',
     function($scope, $routeParams, $resource) {
         var Curso = $resource('/cursos/:id');
         if ($routeParams.cursoId) {
-
             Curso.get({ id: $routeParams.cursoId },
                 function(curso) {
                     $scope.curso = curso;
                 },
                 function(erro) {
-                    $scope.mensagem = {
-                        texto: 'Não foi possível obter o curso.'
-                    };
+                    $scope.mensagem = { texto: 'Não foi possível obter o curso.' };
                     console.log($routeParams.cursoId);
                 }
             );
